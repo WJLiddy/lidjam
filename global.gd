cc5a05f06d06af997140d6f5206bf93695250f9e
Anon <>
2025-11-01 12:05:09 -0700

    viewports for better viewporting

---
+++ global.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var pics = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func add_pic(pic: Image):
+	pics.push_front(pic)
+	
+	
