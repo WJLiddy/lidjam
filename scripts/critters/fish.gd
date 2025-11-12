cefbb8993c0cd85b51077fee27d3d2156d80f880
Anon <>
2025-11-11 23:17:05 -0800

    fish

--- scripts/critters/fish.gd
+++ scripts/critters/fish.gd
@@ -1,14 +1,19 @@
 extends Critter
 @export var follow : PathFollow3D
+@export var is_lead : bool
 
+var vel = 0
+var pos = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if(species == "fishguy"):
-		follow.progress += delta * 60
-		global_position = follow.global_position
-		return
+	
+func _physics_process(delta: float) -> void:
+	if(is_lead):
+		follow.progress += delta * 10
+	if(randf_range(0,1.5) < delta and pos == 0):
+		vel = 0.5
+	vel = vel-delta
+	pos = max(0,pos + vel)
+	global_position = follow.global_position + Vector3(0,pos,0)
+	return
