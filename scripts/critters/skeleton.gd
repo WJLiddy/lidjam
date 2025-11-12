534f67bd1acb48a275334193163f71660c6469d2
Anon <>
2025-11-11 16:11:55 -0800

    skelal

--- scripts/critters/skeleton.gd
+++ scripts/critters/skeleton.gd
@@ -1,5 +1,19 @@
 extends Critter
 
+var bugle = false
 
-func _physics_process(delta: float) -> void:
-	pass
+# bugling, hiding, peeking
+func pick_action():
+	if(bugle):
+		action = "Bugling"
+	elif(dist_to_player() < 10):
+		action = "Hiding"
+	else:
+		action = "Peeking"
+	action_time = get_anim_length(action)
+	$model/AnimationPlayer.play(action)
+	
+
+
+func _on_spook_zone_body_entered(body: Node3D) -> void:
+	bugle = true
