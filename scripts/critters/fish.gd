025de826d6b531ffb056a0c34207641c32813e40
Anon <>
2025-11-11 23:24:46 -0800

    fish

--- scripts/critters/fish.gd
+++ scripts/critters/fish.gd
@@ -6,14 +6,14 @@ var vel = 0
 var pos = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$model/AnimationPlayer.play("Swim")
 	
 func _physics_process(delta: float) -> void:
 	if(is_lead):
-		follow.progress += delta * 10
+		follow.progress += delta * 8
 	if(randf_range(0,1.5) < delta and pos == 0):
-		vel = 0.5
-	vel = vel-delta
+		vel = 0.3
+	vel = vel-(0.5 * delta)
 	pos = max(0,pos + vel)
 	global_position = follow.global_position + Vector3(0,pos,0)
 	return
--- scripts/critters/fly.gd
+++ scripts/critters/fly.gd
@@ -1,5 +1,14 @@
 extends Critter
 
+var home 
+var targ
+func _ready() -> void:
+	home = global_position
+	targ = global_position
+	
 func _physics_process(delta: float) -> void:
-	# does not use nav mesh. stays near river.
-	pass
+	if(global_position.distance_to(targ) < 1):
+		targ = home + Vector3(randf_range(-5,5),0,randf_range(-5,5))
+	global_position = global_position.move_toward(targ,delta * speed())
+	look_at_grad(delta,targ)
+	
