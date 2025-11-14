5011aa134468e2166280eeddfee95e7b190db7b6
Anon <>
2025-11-13 18:31:30 -0800

    up

--- scripts/critters/deer.gd
+++ scripts/critters/deer.gd
@@ -3,18 +3,21 @@ extends Critter
 func speed():
 	if fleeing:
 		return 15
-	return 12
+	return 5
 
 var fleeing = false
+var alerted = false
 
 # walking grazing petrified resting
 func pick_action():
 	# check if we can stop fleeing
 	if(dist_to_player() > 50):
 		fleeing = false
+		alerted = false
 		
-	if(dist_to_player() * stealth_mult() < 35 and dist_to_player() * stealth_mult() > 30):
+	if((not fleeing) and (not alerted) and dist_to_player() * stealth_mult() < 45 and dist_to_player() * stealth_mult() > 30):
 		make_emoticon("Alert")
+		alerted = true
 		
 	# used the whistle.
 	if((not fleeing) and player_is_whistling() and dist_to_player() < 50):
@@ -25,6 +28,7 @@ func pick_action():
 		action = "Walking"
 		set_nav_flee_from_player()
 	elif(dist_to_player() * stealth_mult() < 30):
+		action = "Walking"
 		make_emoticon("Scared")
 		fleeing = true
 	else:
